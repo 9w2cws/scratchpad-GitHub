5ab4e11b60e1ce68384d8c1a61aec792dd27392b
Vince Chia <9w2cws@gmail.com>
2021-07-26 22:12:24 +0800

    print "NUMBER ONE" from team mate one.

--- scratchpad-GitHub-2019/Source.cpp
+++ scratchpad-GitHub-2019/Source.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /* I am team mate number ONE. */
 int main(void) {
+	
 	printf("\nThis is Main Fucntion\n.");
+	/*team mate one add function */
+	printf("\nNUMBER ONE\n");
+
 	return 0;
 }
